2c494a1ad5adb790833221efe453195dbe5255c7
unknown <calin.timofte@yahoo.com>
2020-05-07 12:39:11 +0300

    Spring initializr.

---
+++ Lab11/demo/src/main/java/info/uaic/ro/programare_avansata/lab11/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package info.uaic.ro.programare_avansata.lab11.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("Hello world!");
+	}
+
+}
---
+++ Lab11/demo/src/test/java/info/uaic/ro/programare_avansata/lab11/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package info.uaic.ro.programare_avansata.lab11.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
